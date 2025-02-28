b7c73d838272c4518e7c7c55dd7652da6250909d
yasuda ryuuya <ysdzm75@gmail.com>
2025-02-28 18:23:53 +0900

    fix: Tanstack Tableでdata.jsonを表示するサンプルを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,49 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>hello,world</h1>
-    </div>
-  );
+import { createColumnHelper, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { ShowTable } from '@/components/ShowTable';
+import { useState, useEffect } from 'react';
+
+// テーブルデータの型を定義
+type TestTable = {
+  hoge: string;
+  fuga: number;
+}
+
+// createColumnHelper を利用してカラム定義を作成
+const columnHelper = createColumnHelper<TestTable>();
+
+const testTableColumnDefs = [
+  columnHelper.accessor((row) => row.hoge, {
+    id: 'hoge',
+    header: 'hoge',
+  }),
+  columnHelper.accessor((row) => row.fuga, {
+    id: 'fuga',
+    header: 'fuga',
+  }),
+];
+
+export default function Page() {
+  const [data, setData] = useState<TestTable[]>([]);
+
+  useEffect(() => {
+    // 仮のJSONデータを読み込む
+    const fetchData = async () => {
+      const response = await fetch('/data.json'); // データのパスを指定
+      const jsonData: TestTable[] = await response.json();
+      setData(jsonData);
+    };
+    
+    fetchData();
+  }, []);
+
+  // useReactTable 呼び出し
+  const table = useReactTable<TestTable>({
+    columns: testTableColumnDefs,
+    data: data,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return <ShowTable table={table} />;
 }
